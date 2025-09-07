e94c96f7eef4e1066e56674ec1bb24a234e79aae
Sejung Yoo <sjyu95@gmail.com>
2025-09-07 09:52:57 +0900

    Test for standard functor

--- 42.FunctionLikeEntities/42.7StandardFunctors/main.cpp
+++ 42.FunctionLikeEntities/42.7StandardFunctors/main.cpp
@@ -1,104 +1,51 @@
-#include <iostream>
 #include <functional>
+#include <iostream>
 #include "boxcontainer.h"
 
-
-
-template <typename T, typename Comparator>
-T get_best (const BoxContainer<T>& collection,
-                            Comparator comparator){
-    T best = collection.get_item(0);
-    for(size_t i{}; i < collection.size() ; ++i){
-            
-        if(comparator(collection.get_item(i),best)){
-            best = collection.get_item(i);
-        }
-    }    
+template<typename T, typename Comparator>
+const T& get_best(const BoxContainer<T>& container, Comparator comp) {
+    T& best = container.get_item(0);
+    for (size_t i{1}; i < container.size(); ++i) {
+        if (comp(container.get_item(i), best))
+            best = container.get_item(i);
+    }
     return best;
 }
 
-//Custom function
+// custom functor
 template <typename T>
-bool custom_greater(const T& param1, const T& param2){
-     if(param1 > param2){
-         return true;
-     }
-     return false;
-}
-
-//Custom functor
-template <typename T>
-class Greater{
-    public : 
-    bool operator()(const T& param1, const T& param2){
-        return (param1 > param2) ? true : false;
+class Greater {
+public:
+    bool operator() (const T& a, const T& b) {
+        return a>b;
     }
 };
 
+// custom function
+template <typename T>
+bool Greate(const T& a, const T& b) {
+    return a > b;
+}
 
+int main(int argc, char** argv) {
+    std::plus<int> add;
+    std::minus<int> minus;
+    std::greater<int> greater_comp;
 
-int main(){
-
-    std::plus<int> adder;
-    std::minus<int> substracter;
-    std::greater<int> compare_greater;
-    
+    std::cout << "std functor plus 1+2 = " << add(1,2) << std::endl;
+    std::cout << "std functor minus 10-2 = " << minus(10,2) << std::endl;
     std::cout << std::boolalpha;
-    std::cout << " 10 + 7 : " << adder(10,7) << std::endl; //17
-    
-    std::cout << "10 - 7 : "  << substracter(10,7) << std::endl;//3
-    
-    std::cout << " 10 > 7 : " << compare_greater(10,7) << std::endl;//true
-    
-    std::cout << "---------------------------------" << std::endl;
+    std::cout << "std functor greater 10 > 2 is " << greater_comp(10,2) << std::endl;
 
+    BoxContainer<std::string> boxes{};
+    boxes.add("The ");
+    boxes.add("sky ");
+    boxes.add("is ");
+    boxes.add("blue. ");
 
-    BoxContainer<std::string> quote;
-    quote.add("The");
-    quote.add("sky");
-    quote.add("is");
-    quote.add("blue");
-    quote.add("my");
-    quote.add("friend");
-  
-    
-    std::greater<std::string> string_comparator{};
-    
-    std::cout << "quote : " << quote << std::endl;
-    //Built in functor
-    std::cout << "greater string : " <<
-            get_best(quote,string_comparator) << std::endl;
-    //Custom function pointer
-    std::cout << "greater string : " 
-            << get_best(quote,custom_greater<std::string>) << std::endl;
-    //Custom functor
-    Greater<std::string> greater_string_custom_functor;
-    std::cout << "greater string : "
-        << get_best(quote,greater_string_custom_functor) << std::endl;
+    std::cout << "get best box item using custom functor is " << get_best<std::string>(boxes, Greater<std::string>{}) << std::endl;
+    std::cout << "get best box item using std::greater is " << get_best<std::string>(boxes, std::greater<std::string>{}) << std::endl;
+    std::cout << "get best box item uaing custom function pointer is " << get_best<std::string>(boxes, Greate<std::string>) << std::endl;
 
-
-    std::cout << "---------------------------------" << std::endl;
-
-   BoxContainer<int> ints;
-    ints.add(10);
-    ints.add(3);
-    ints.add(6);
-    ints.add(72);
-    ints.add(23);
-    ints.add(4);
- 
-    
-    std::greater<int> int_comparator{};
-    Greater<int> greater_int_custom_functor;
-    
-    std::cout << "ints : " << ints << std::endl;
-    std::cout << "greater int : "
-        << get_best(ints,int_comparator) << std::endl; 
-    std::cout << "greater int : "
-        << get_best(ints , custom_greater<int>) << std::endl;
-    std::cout << "greater int : "
-        << get_best(ints,greater_int_custom_functor) << std::endl;
-    std::cout << "lesser int : " << get_best(ints,std::less<int>{}) << std::endl;
-    
     return 0;
 }
