cbc11300c96ed772885abf168ab2287545d3f1b7
Sejung Yoo <sjyu95@gmail.com>
2025-09-28 08:47:25 +0900

    Test for functions and lambda function

--- 42.FunctionLikeEntities/42.9FunctorsAndLambdaFunctions/main.cpp
+++ 42.FunctionLikeEntities/42.9FunctorsAndLambdaFunctions/main.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-int main(){
+int main() {
+    std::cout << [](int a, int b) -> int { return a + b; }(7,3) << std::endl;
 
-    int result = [] (int x, int y) -> int { return x + y; }(7,3);
-    std::cout << result << std::endl;
-
-
-    //Auto type deduction can help deduce the type of the generated lambda function
-    //We don't have access to that in our C++ code.
-    auto func = [] (int x, int y) -> int { return x + y; };
-    result = func(10,20);
-    std::cout << result  << std::endl;
-   
-    return 0;
+    auto result = [](int a, int b) -> int { return a + b; };
+    std::cout << result(2,4) << std::endl;
 }
