e52d17585b79f90b3adfea9ef92dbcc98765278f
Sejung Yoo <sjyu95@gmail.com>
2025-10-08 22:20:43 +0900

    Test for lamda as callback

--- 42.FunctionLikeEntities/42.10LambdaFunctionsAsCallbacks/main.cpp
+++ 42.FunctionLikeEntities/42.10LambdaFunctionsAsCallbacks/main.cpp
@@ -1,112 +1,79 @@
 #include <iostream>
 #include "boxcontainer.h"
 
+auto encrypt = [](const char & a) {
+    return static_cast<char>(a+3);
+};
 
-std::string & modify(std::string& str_param,
-                                        char(* modifier)(const char&))
-{
-     for(size_t i{} ; i < str_param.size() ; ++i){
-        str_param[i] = modifier(str_param[i]); // Calling the callback
-     }
-     return str_param;
+auto decrypt = [](const char & a) {
+    return static_cast<char>(a-3);
+};
+
+std::string& modify(std::string& string_param, char(*modifier)(const char &)) {
+    for(size_t i{}; i < string_param.size(); ++i) {
+        string_param[i] = modifier(string_param[i]);
+    }
+    return string_param;
 }
 
-
-//Modifying a BoxContainer of strings
-BoxContainer<std::string>& modify(BoxContainer<std::string>& sentence,
-                                        char(*modifier) (const char&)){
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
-        //Code below relies on get_item() to return a reference
-        //Loop through the word modifying each character 
-        for(size_t j{} ; j < sentence.get_item(i).size(); ++j){
+BoxContainer<std::string>& modify(BoxContainer<std::string>& sentence, char(*modifier)(const char &)) {
+    for(size_t i{}; i < sentence.size(); ++i) {
+        for(size_t j{}; j < sentence.get_item(i).size(); ++j) {
             sentence.get_item(i)[j] = modifier(sentence.get_item(i)[j]);
         }
     }
     return sentence;
 }
 
-
-std::string get_best (const BoxContainer<std::string>& sentence,
-                        bool(*comparator)(const std::string& str1, const std::string& str2)){
-
+std::string get_best(BoxContainer<std::string>& sentence, bool(*comp)(std::string& a, std::string& b)) {
     std::string best = sentence.get_item(0);
-    for(size_t i{}; i < sentence.size() ; ++i){
-            
-        if(comparator(sentence.get_item(i),best)){
+    for(size_t i{1}; i < sentence.size(); ++i) {
+        if(comp(sentence.get_item(i), best)) {
             best = sentence.get_item(i);
         }
-            
     }
-        
     return best;
 }
 
+int main(int argc, char** argv) {
+    std::cout << "string encrypt decrypt: " << std::endl;
+    std::string hello{"hello"};
+    auto enc = modify(hello, encrypt);
+    std::cout << "encrypt hello is " << enc << std::endl;
+    std::cout << "decrypt is " << modify(enc, decrypt) << std::endl;
 
+    std::cout << "-------------" << std::endl;
+    std::string hello1{"hello"};
+    auto enc1 = modify(hello1, [](const char & a){return static_cast<char>(a+3);});
+    std::cout << "encrypt hello is " << enc1 << std::endl;
+    std::cout << "decrypt is " << modify(enc1, [](const char & a){return static_cast<char>(a-3);}) << std::endl;
 
-int main(){
+    std::cout << "-------------" << std::endl;
+    BoxContainer<std::string> box;
+    box.add("The ");
+    box.add("sky ");
+    box.add("is ");
+    box.add("blue ");
+    auto enc2 = modify(box, encrypt);
+    std::cout << "encrypt box is " << enc2 << std::endl;
+    std::cout << "decrypt is " << modify(enc2, decrypt) << std::endl;
 
-    std::string str {"Hello"};
-    
-    auto encrypt = [](const char& param){ // Callback function
-        return static_cast<char> (param + 3);
+    auto larger_in_size = [](std::string& a, std::string& b) {
+        return (a.size() > b.size());
     };
-    
-    auto decrypt = [](const char& param){ // Callback function
-        return static_cast<char> (param - 3);
+    auto greator_lexcogiographical = [](std::string& a, std::string& b) {
+        return (a > b);
     };
+    std::cout << "-------------" << std::endl;
+    BoxContainer<std::string> box2;
+    box2.add("The ");
+    box2.add("sky ");
+    box2.add("is ");
+    box2.add("blue ");
+    auto best = get_best(box2, larger_in_size);
+    std::cout << "larger in size : " << best << std::endl;
+    best = get_best(box2, greator_lexcogiographical);
+    std::cout << "greator lexicogeographly : " << best << std::endl;
 
-    //Modifying through callbacks.
-    std::cout << "Initial : " << str << std::endl;
-    std::cout << "Encrypted : " <<  modify(str,encrypt) << std::endl;
-    std::cout << "Decrypted : " << modify(str,decrypt) << std::endl;
-
-    std::cout << "---------" << std::endl;
-
-    //Passing lambda functions directly
-    std::cout << "Initial : " << str << std::endl;
-    std::cout << "Encrypted : " <<  modify(str,[](const char& param){ // Callback function
-        return static_cast<char> (param + 3);
-    }) << std::endl;
-    std::cout << "Decrypted : " << modify(str,[](const char& param){ // Callback function
-        return static_cast<char> (param - 3);
-    }) << std::endl;
-
-
-    std::cout << "--------" << std::endl;
-
-    std::cout << std::endl;
-    std::cout << "strings stored in BoxContainer : " << std::endl;
-    BoxContainer<std::string> quote;
-    quote.add("The");
-    quote.add("sky");
-    quote.add("is");
-    quote.add("blue");
-    quote.add("my");
-    quote.add("friend");
-    std::cout << "Initial : " <<  quote << std::endl;
-    std::cout << "Encrypted : " << modify(quote,encrypt) << std::endl;
-    std::cout << "Decrypted : " << modify(quote,decrypt) << std::endl;
-
-
-    std::cout << "--------" << std::endl;
-
-   auto larger_in_size = [] (const std::string& str1, const std::string& str2){
-                            if(str1.size() > str2.size())
-                                return true;
-                            else 
-                                return false;
-                        };
-                        
-    auto greater_lexicographically = [](const std::string& str1, const std::string& str2){
-                                        return (str1>str2);
-                                    };
-
-    std::cout << std::endl;
-    std::cout << "Gettting the best : " << std::endl;
-    std::cout << "larger in size : " << get_best(quote,larger_in_size) << std::endl;
-    std::cout << "greater lexicographicaly : " 
-            << get_best(quote,greater_lexicographically) << std::endl;
-    
     return 0;
 }
