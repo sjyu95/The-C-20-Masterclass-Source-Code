87c5ecb3ddfeca8dc04ece7a3fadf8489f7bb4d2
Sejung Yoo <sjyu95@gmail.com>
2025-10-19 20:11:10 +0900

    Test ffor lamda value capture

--- 42.FunctionLikeEntities/42.11CapturingByValueUnderTheHood/main.cpp
+++ 42.FunctionLikeEntities/42.11CapturingByValueUnderTheHood/main.cpp
@@ -1,31 +1,34 @@
 #include <iostream>
 
+int main() {
+    int a{1};
+    int b{2};
+    int some_val{10};
+    int some_val1{20};
 
-int main(){
-
-    int a{7};
-    int b {3};
-    int some_var{28};// Not captured by the [=] lambda, so it won't set up a member var 
-                    // for this
-    double some_other_var{55.5}; // Not captured by the [=] lambda, so it won't set up 
-                                // a member var for this.
-
-    //Capturing  everything by value 
-    auto func = [=] (int c, int d)  {
-        std::cout << "Captured values : " << std::endl;
-        std::cout << "a : " << a << std::endl;
-        std::cout << "b : " << b  << std::endl;
-        
-        std::cout << std::endl;
-        
-        std::cout << "Parameters : " << std::endl;
-        std::cout << "c : " << c << std::endl;
-        std::cout << "d : " << d << std::endl;
+    auto func = [a, b](int c, int d) {
+        std::cout << "captured value a : " << a << ", b: " << b << std::endl;
+        std::cout << "parameter c : " << c << ", d: " << d << std::endl;
     };
-    func(10,20);
+    func(100,200);
 
-    std::cout << "----" << std::endl;
-    func(20,30);
-   
+    std::cout << "-------------" << std::endl;
+
+    auto func1 = [=](int c, int d) {
+        std::cout << "captured value a : " << a << ", b: " << b << std::endl;
+        std::cout << "parameter c : " << c << ", d: " << d << std::endl;
+    };
+    func1(101,201);
+
+    std::cout << "-------------" << std::endl;
+
+    auto func2 = [=](int c, int d) mutable {
+        a++;
+        std::cout << "captured value a : " << a << ", b: " << b << std::endl;
+        std::cout << "parameter c : " << c << ", d: " << d << std::endl;
+    };
+    func2(300,400);
+    func2(301,401);
+
     return 0;
 }
